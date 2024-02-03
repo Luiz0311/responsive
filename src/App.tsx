575842263b728adb4406102de68fb590fb7e69bf
Luiz0311 <luizhenriquediogo7@gmail.com>
2024-02-03 15:02:50 -0300

    Bug: fixed
    just a useeffect.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,23 @@ function Navbar() {
   ]
   const [showMenu, setShowMenu] = useState(false)
 
+  useEffect(() => {
+    const handler = () => {
+      if (!isScreenSmall()) setShowMenu(false)
+    }
+
+    window.addEventListener("resize", handler)
+
+    return () => {
+      window.removeEventListener("resize", handler)
+    }
+  }, [])
+
+
+  function isScreenSmall() {
+    return window.innerWidth < 640
+  }
+
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
